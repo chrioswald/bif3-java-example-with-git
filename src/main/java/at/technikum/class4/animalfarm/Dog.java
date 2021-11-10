a68677dc2d269add803881b5ca0d3c04175fa04f
chrioswald <94034937+chrioswald@users.noreply.github.com>
2021-11-10 09:09:20 +0100

    add treats for dogs (#2)

--- src/main/java/at/technikum/class4/animalfarm/Dog.java
+++ src/main/java/at/technikum/class4/animalfarm/Dog.java
@@ -1,9 +1,22 @@
 package at.technikum.class4.animalfarm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dog extends BaseAnimal implements SpeakingAnimal {
 
     public Dog() {
         super("Dino");
+    }
+
+    private List<String> treats = new ArrayList<>();
+
+    public List<String> getTreats() {
+        return treats;
+    }
+
+    public void printTreats() {
+        System.out.println(treats);
     }
 
     public void speak() {
