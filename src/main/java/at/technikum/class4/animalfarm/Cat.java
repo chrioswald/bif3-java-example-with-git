1ed540ee287b62065d765f9ba1f990bfd8247c91
Christoph Oswald <christoph.oswald@bearingpoint.com>
2021-11-10 08:31:03 +0100

    [myticket-1234] add cat and dog

---
+++ src/main/java/at/technikum/class4/animalfarm/Cat.java
@@ -0,0 +1,7 @@
+package at.technikum.class4.animalfarm;
+
+public class Cat {
+    public void speak() {
+        System.out.println("Meow meow!");
+    }
+}
---
+++ src/main/java/at/technikum/class4/animalfarm/Dog.java
@@ -0,0 +1,7 @@
+package at.technikum.class4.animalfarm;
+
+public class Dog {
+    public void speak() {
+        System.out.println("Bark bark!");
+    }
+}
